05bde7db2bc7421290bfb09c6fa3f03340734935
Juan C <121655192+DevJuanzok4@users.noreply.github.com>
2023-08-19 20:50:44 +0000

    chore: adiciona estrutura condicional

--- app.rb
+++ app.rb
@@ -1,2 +1,11 @@
 print "\e[33mVocê quer um copo de café?\e[0m\n"
-puts "(Digite s/n)"
+puts "Digite (s/n)\n"
+resposta = gets.chomp.downcase
+if resposta == s
+    puts "~Você pega o copo de café e toma."
+elsif resposta == n 
+    puts "Você ~ Não, não, obrigado."
+else 
+    puts "Você vai querer o café ou não?"
+    puts "Digite s/n"
+end
